c85105fece6d47065ddb1fc6d5a90021002041c8
Oleksandr Frentsel <Oleksandr.Frentsel@sophos.com>
2019-03-27 18:03:48 +0200

    fixed unit-test

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
+import createSagaMiddleware from 'redux-saga';
+import { reducer as toastrReducer } from 'react-redux-toastr';
+import rootSaga from './saga';
+
 import App from './App';
+import AppReducer from './parts/reducers/app';
+
+const reducer = combineReducers({
+  app: AppReducer,
+  toastr: toastrReducer
+});
+
+const sagaMiddleware = createSagaMiddleware();
+const store = createStore(
+  reducer,
+  applyMiddleware(sagaMiddleware)
+);
+
+sagaMiddleware.run(rootSaga);
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(
+    <Provider store={store}>
+      <App />
+    </Provider>, div
+  );
   ReactDOM.unmountComponentAtNode(div);
 });
